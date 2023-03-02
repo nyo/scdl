4038778b338370864767f8de65ea2fab07b0c736
nyo <nyo@users.noreply.github.com>
2023-03-02 16:26:55 +0100

    refactor/improve/fix main code

--- index.js
+++ index.js
@@ -1,250 +1,338 @@
-window.BASE_URL = "https://api-v2.soundcloud.com";
-window.CLIENT_ID = null;
-window.ELEMENTS = [];
-window.LAST_URL = "";
-window.TRACKS_COUNTER = 0;
+window.SCDL__CLIENT_ID = null;
+window.SCDL__LAST_URL = null;
+window.SCDL__TRACK_COUNT = 0;
+window.SCDL__DOM_ELEMENTS = [];
 
-const watchNewTracks = setInterval(function() {
-	/* check every second for new loaded tracks in the page */
-	let nbShareButtons = document.querySelectorAll(".sc-button-share").length;
-	if (nbShareButtons > 0 && nbShareButtons != window.TRACKS_COUNTER
-		|| window.LAST_URL != document.URL) {
-		insertButtons();
-		window.TRACKS_COUNTER = nbShareButtons;
-	}
-	window.LAST_URL = document.URL;
+/**
+ * Check every second for new loaded tracks
+ * in the page to add download button to
+ */
+const watchNewTracksInterval = setInterval(() => {
+  try {
+    const currentUrl = document.URL;
+    const nbShareButtons = document.querySelectorAll(".sc-button-share").length;
+
+    if (
+      window.SCDL__CLIENT_ID
+          && (currentUrl !== window.SCDL__LAST_URL
+          || nbShareButtons !== window.SCDL__TRACK_COUNT)
+    ) {
+      insertDownloadButtons();
+      window.SCDL__TRACK_COUNT = nbShareButtons;
+      window.SCDL__LAST_URL = currentUrl;
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }, 1000);
 
-function logger(_level, _function, _details) {
+/**
+ * Stop the interval when the user navigates away from the page
+ */
+window.addEventListener("beforeunload", () => {
+  clearInterval(watchNewTracksInterval);
+});
 
-	const logging = [
-		{ "level": 0, "word": "UNKNOWN"	, "function": console.log	},
-		{ "level": 1, "word": "TRACE"	, "function": console.log	},
-		{ "level": 2, "word": "DEBUG"	, "function": () => {}		},
-		{ "level": 3, "word": "INFO"	, "function": console.info	},
-		{ "level": 4, "word": "WARN"	, "function": console.warn	},
-		{ "level": 5, "word": "ERROR"	, "function": console.error	}
-	];
+/**
+ * Write metadatas to track buffer and save it as mp3 file.
+ * @param {object} metadata 
+ * @param {ArrayBuffer} trackBuffer 
+ * @param {ArrayBuffer} artworkBuffer 
+ */
+const tagAndSaveTrack = (metadata, trackBuffer, artworkBuffer) => {
+  const writer = new ID3Writer(trackBuffer);
 
-	let lvl = (_level >= 1 && _level <= 5) ? _level : 0;
-
-	if (typeof _function !== "string") _function = "_unknown_";
-	if (typeof _details !== "string") _details = "_no_details_";
-
-	let msg = `[SCDL] ${logging[lvl].word} | ${_function} | ${_details}`;
-	logging[lvl].function(msg);
-
-};
-
-async function tagAndDownload(metadata, mp3ArrayBuffer, artworkArrayBuffer) {
-
-	/* write id3 tags */
-	const writer = new ID3Writer(mp3ArrayBuffer);
-	writer
-		.setFrame("TPE1", [metadata.artist])
+  writer
+    .setFrame("TPE1", [metadata.user?.username])
 		.setFrame("TIT2", metadata.title)
-		.setFrame("TYER", metadata.year)
+		.setFrame("TYER", metadata.created_at?.split("-")[0]) // keep only year from date
 		.setFrame("TCON", [metadata.genre])
-		.setFrame("COMM", {
-			"description": "Track URL",
-			"text": metadata.comment
-		})
+		.setFrame("WOAS", metadata.permalink_url)
 		.setFrame("APIC", {
-			"type": 3,
-			"data": artworkArrayBuffer,
-			"description": "Track artwork"
-	});
-	writer.addTag();
+			type: 3,
+			data: artworkBuffer,
+			description: "Track artwork"
+	  });
 
-	/* save file as... */
-	const blob = writer.getBlob();
-	const filename = `${metadata.artist.toLowerCase()} - ${metadata.title.toLowerCase()}.mp3`;
-	saveAs(blob, filename);
+  writer.addTag();
+  
+  saveAs(
+    writer.getBlob(),
+    `${metadata.user?.username} - ${metadata.title}.mp3`.toLowerCase()
+  );
+
 	writer.revokeURL(); // memory control
-
 };
 
-async function resolveTrack(track_url) {
+/**
+ * Find a working transcoding for the track and return it.
+ * @param {object[]} transcodings 
+ * @returns {Promise<any>}
+ */
+const getStreamData = async (transcodings) => {
+  // prioritizes 'progressive' transcoding format
+  transcodings.sort((a, b) => {
+    if (a.format.protocol === "progressive") return -1;
+    if (b.format.protocol === "progressive") return 1;
+    return 0;
+  });
 
-	const url = `${window.BASE_URL}/resolve?url=${track_url}&client_id=${window.CLIENT_ID}`;
-	logger(3, "resolveTrack", `Downloading "${url}" ...`);
+  for (const transcoding of transcodings) {
+    console.info(`Trying with '${transcoding.format?.protocol}' transcoding ('${transcoding.format?.mime_type}' MIME type)...`);
 
-	try {
+    const streamUrl = new URL(transcoding.url);
+    
+    streamUrl.searchParams.set("client_id", window.SCDL__CLIENT_ID);
 
-		/* get track data (informations, artwork, stream url) */
-		const track_metadata = await fetch(url);
-		const { user, title, created_at, genre,
-			permalink_url, artwork_url, media } = await track_metadata.json();
+    const streamRes = await fetch(streamUrl.toString());
 
-		const transcoding_metadata = media.transcodings.find((transcoding) => transcoding.format.protocol === "progressive");
-		if (!transcoding_metadata) throw "cannot download this track (invalid transcoding)";
+    if (streamRes.status === 200) return streamRes.json();
+  }
 
-		const metadata = {
-			"artist": user.username,
-			"title": title,
-			"year": created_at.split("-")[0], // get only year from date
-			"genre": genre,
-			"comment": permalink_url,
-			"artwork_url": (artwork_url || user.avatar_url).replace(/large/ig, "t500x500"),
-			"stream_url": `${transcoding_metadata.url}?client_id=${window.CLIENT_ID}`
-		};
-		logger(2, "resolveTrack", JSON.stringify(metadata, null, 4));
-
-		/* get mp3 (128kbps) direct url */
-		const mp3_data = await fetch(metadata.stream_url);
-		const { url: mp3_url } = await mp3_data.json();
-		logger(2, "resolveTrack", JSON.stringify(mp3_url, null, 4));
-
-		/* download mp3 & store it in a buffer */
-		const mp3 = await fetch(mp3_url);
-		const mp3ArrayBuffer = await mp3.arrayBuffer();
-
-		/* download artwork & store it in a buffer */
-		const artwork = await fetch(metadata.artwork_url);
-		const artworkArrayBuffer = await artwork.arrayBuffer();
-
-		tagAndDownload(metadata, mp3ArrayBuffer, artworkArrayBuffer);
-
-	} catch (error) {
-
-		logger(5, "resolveTrack", error.message ? error.message : error);
-
-	}
-
+  throw new Error(`Couldn't get stream data from transcoding URL...`);
 }
 
-function isLinkElem(className) {
-	return className.indexOf("playableTile__mainHeading") !== -1
-		|| className.indexOf("soundTitle__title") !== -1
-		|| className.indexOf("trackItem__trackTitle") !== -1;
-}
+/**
+ * Resolve track metadata and buffers for download.
+ * @param {string} url 
+ * @returns {object}
+ */
+const resolveTrack = async (url) => {
+  const resolveUrl = new URL("https://api-v2.soundcloud.com");
 
-function getTrackURI(element) {
+  resolveUrl.pathname = "resolve";
+  resolveUrl.searchParams.set("url", url);
+  resolveUrl.searchParams.set("client_id", window.SCDL__CLIENT_ID);
 
-	try {
+  const resolveRes = await fetch(resolveUrl.toString());
 
-		let node = element.closest(".streamContext")
-			|| element.closest(".chartTrack")
-			|| element.closest(".trackItem");
+  if (resolveRes.status !== 200) {
+    throw new Error(`Error while resolving '${url}'...`);
+  }
 
-		let track_url;
-		if (node) {
-			let links = node.querySelectorAll("a");
-			let len = 0;
-			let best_link;
-			/* look for longest link */
-			for (let i = 0; i < links.length; i++) {
-				if (links[i].href && links[i].href.length > len
-					&& isLinkElem(links[i].className)
-					&& links[i].href.indexOf("/stream") === -1
-					&& links[i].href.indexOf("/comments") === -1) {
-						len = links[i].href.length;
-						best_link = links[i];
-				}
-			}
-			track_url = best_link.href.split("?")[0];
-			if (track_url.indexOf("soundcloud.com") === -1) {
-				track_url = "https://soundcloud.com" + track_url;
-			}
-		} else {
-			track_url = document.URL;
-		}
+  const resolveData = await resolveRes.json();
+  const streamData = await getStreamData(resolveData?.media?.transcodings);
+  
+  const trackUrl = new URL(streamData.url);
+  trackUrl.searchParams.set("client_id", window.SCDL__CLIENT_ID);
 
-		logger(2, "getTrackURI", `track_url: ${track_url}`);
-		resolveTrack(track_url);
+  const artworkUrl = (resolveData?.artwork_url || resolveData?.user?.avatar_url)
+    .replace(/large/ig, "t500x500");
 
-	} catch (error) {
+  const [
+    trackRes,
+    artworkRes
+  ] = await Promise.all([
+    fetch(trackUrl.toString()),
+    fetch(artworkUrl)
+  ]);
 
-		logger(5, "getTrackURI", error.message);
+  if (trackRes.status !== 200 || artworkRes.status !== 200) {
+    throw new Error(`Error while fetching track/artwork data...`);
+  }
 
-	}
+  const [
+    trackBuffer,
+    artworkBuffer
+  ] = await Promise.all([
+    trackRes.arrayBuffer(),
+    artworkRes.arrayBuffer()
+  ]);
 
+  return { resolveData, trackBuffer, artworkBuffer };
 };
 
-function insertButtons() {
+/**
+ * Checks whether the given link is valid or not
+ * @param {HTMLAnchorElement} link 
+ * @returns {boolean}
+ */
+const isValidLink = (link) => {
+  if (!link?.href) return false;
 
-	logger(2, "insertButtons", "! INSERTING NEW BUTTONS !");
+  const validClassNames = [
+    "trackItem__trackTitle",
+    "soundTitle__title",
+    "chartTrack__title",
+    "playableTile__mainHeading"
+  ];
 
-	/* check if the button should be medium depending on the page */
-	const url = document.URL.split("/");
-	const reserved_strings = ["tracks", "sets", "albums", "reposts", "toptracks", "popular-tracks"];
-	const should_be_medium = url[4] && url[4].length > 0
-		&& !reserved_strings.includes(url[4]) ? true : false;
+  if (!validClassNames.some((className) => link.className?.includes(className))) {
+    return false;
+  }
 
-	/* create the html element */
-	const downloadButton = document.createElement("a");
-	const buttonText = document.createTextNode("Download");
-	downloadButton.appendChild(buttonText);
-	downloadButton.title = "Download";
-	downloadButton.className = `sc-button-download sc-button sc-button-${should_be_medium ? "medium" : "small"} sc-button-responsive`;
+  const ignoredPaths = [
+    "/stream",
+    "/comments"
+  ];
 
-	try {
-
-		const elems = document.getElementsByTagName("*");
-		for (e in elems) {
-			/* get all parent nodes */
-			if (elems[e] && typeof elems[e].className === "string" && elems[e].className.indexOf("sc-button-group") > -1) {
-				/* check for duplicates (prevents multiple buttons) */
-				if (window.ELEMENTS.includes(elems[e].parentNode)) continue;
-				/* clone and append a download button */
-				let kiddies = Array.from(elems[e].childNodes);
-				kiddies = kiddies.filter((k) => k.className);
-				/* don't insert download button on elements with <4 classNames
-				   it means it is a recommended song on the side */
-				if (kiddies.length < 4) continue;
-				let downloadButtonClone = downloadButton.cloneNode(true);
-				downloadButtonClone.addEventListener("click", function () {
-					getTrackURI(this);
-				}, true);
-				elems[e].appendChild(downloadButtonClone);
-				window.ELEMENTS.push(elems[e].parentNode);
-			}
-		}
-
-	} catch (error) {
-
-		logger(5, "insertButtons", error.message);
-
-	}
-
-	// console.debug(window.ELEMENTS);
-
-}
-
-async function getClientID() {
-
-	const scripts = document.getElementsByTagName("script");
-	const urls = Array.from(scripts).reduce((urls, url) => {
-		if (url.src.match(/sndcdn.com\/assets\/[0-9][0-9]*/g))
-			urls.push(url.src);
-		return urls;
-	}, []);
-
-	try {
-
-		/* get custom client_id for the user's session */
-		for (const url in urls) {
-			logger(2, "getClientID", `fetching: '${urls[url]}'`);
-			let response = await fetch(urls[url]);
-			let data = await response.text();
-			window.CLIENT_ID = data.match(new RegExp(",client_id:\"(.*)\",env:\"production\""));
-			if (window.CLIENT_ID) {
-				window.CLIENT_ID = window.CLIENT_ID.filter(str => str.length === 32)[0];
-				break;
-			}
-		}
-
-		logger(2, "getClientID", `Got client_id: '${window.CLIENT_ID}'`);
-
-		/* now that the id has been retrieved, insert download button(s) */
-		insertButtons();
-
-	} catch (error) {
-
-		logger(5, "getClientID", error.message);
-
-	}
-
+  return !ignoredPaths.some((path) => link.href.includes(path));
 };
 
-getClientID();
+/**
+ * Get URL of the track linked to the download button.
+ * @param {HTMLAnchorElement} buttonElement
+ * @returns {string}
+ */
+const getTrackURL = (buttonElement) => {
+  const node = [
+    ".streamContext",
+    ".chartTrack",
+    ".trackItem"
+  ]
+    .map((selector) => buttonElement.closest(selector))
+    .find(Boolean);
+
+  if (!node) return document.URL;
+
+  const links = node.querySelectorAll("a");
+  const longestValidLink = Array.from(links).reduce(
+    (acc, link) => isValidLink(link) && link.href.length > acc.href.length
+      ? link
+      : acc,
+    { href: "" }
+  );
+
+  if (!longestValidLink.href) {
+    throw new Error("Couldn't find track URL...");
+  }
+
+  const trackURL = longestValidLink.href.split("?")[0];
+
+  return trackURL.startsWith("https://soundcloud.com")
+    ? trackURL
+    : "https://soundcloud.com" + trackURL;
+};
+
+/**
+ * Get track URL, resolve its metadata, tag and save as .mp3 file.
+ * @param {HTMLElement} buttonElement 
+ */
+const downloadTrack = async (buttonElement) => {
+  const trackURL = getTrackURL(buttonElement);
+  const { resolveData, trackBuffer, artworkBuffer } = await resolveTrack(trackURL);
+
+  tagAndSaveTrack(resolveData, trackBuffer, artworkBuffer);
+};
+
+/**
+ * Checks whether the given button group should be
+ * appended a child download button.
+ * Skip duplicates, and groups that are not directly linked to a track.
+ * @param {HTMLElement} buttonGroup 
+ * @returns {boolean}
+ */
+const isValidButtonGroup = (buttonGroup) => {
+  if (!buttonGroup) return false;
+
+  const childButtonNodes = Array.from(buttonGroup.childNodes)
+    .filter((node) => node.className);
+
+  const [
+    isSet,
+    isSideTrack,
+    isUserProfile
+  ] = [
+    childButtonNodes[4]?.classList?.contains("addToNextUp"),
+    childButtonNodes.length === 2
+      && childButtonNodes[0]?.classList?.contains("sc-button-like"),
+    childButtonNodes.some((node) =>
+      node.classList?.contains("sc-button-startstation"))
+  ];
+  
+  return !isSet && !isSideTrack && !isUserProfile
+    && !window.SCDL__DOM_ELEMENTS.includes(buttonGroup.parentNode);
+}
+
+/**
+ * Insert 'Download' button(s) in the DOM wherever there is a group
+ * of buttons that appear to be linked to a track.
+ * The size of each button is determined from the size of the buttons of the group.
+ */
+const insertDownloadButtons = () => {
+  // create html anchor element
+  const downloadButton = document.createElement("a");
+  const downloadButtonText = document.createTextNode("Download");
+
+  downloadButton.appendChild(downloadButtonText);
+  downloadButton.setAttribute("title", "Download");
+  downloadButton.classList.add(
+    // "sc-button-disabled",
+    // "sc-button-icon",
+    "sc-button-download",
+    "sc-button",
+    "sc-button-medium", // default button size
+    "sc-button-responsive"
+  );
+
+  // get all button groups in the page
+  const buttonGroups = document.getElementsByClassName("sc-button-group");
+
+  for (const buttonGroup of buttonGroups) {
+    if (!isValidButtonGroup(buttonGroup)) continue;
+
+    const downloadButtonClone = downloadButton.cloneNode(true);
+
+    if (buttonGroup.classList.contains("sc-button-group-small")) {
+      // change button size if needed
+      downloadButtonClone.classList.replace(
+        "sc-button-medium",
+        "sc-button-small"
+      );
+    }
+
+    downloadButtonClone.addEventListener(
+      "click",
+      () => { downloadTrack(downloadButtonClone); },
+      true
+    );
+
+    buttonGroup.appendChild(downloadButtonClone);
+    window.SCDL__DOM_ELEMENTS.push(buttonGroup.parentNode);
+  }
+};
+
+/**
+ * Try to find the SoundCloud clientId for the current
+ * user session, and set it to `window.SCDL__CLIENT_ID`.
+ */
+const setClientId = async () => {
+  const scriptElements = document.getElementsByTagName("script");
+  const scriptSources = Array.from(scriptElements).reduce(
+    (acc, elem) =>
+      elem.src.match(/sndcdn.com\/assets\/[0-9][0-9]*/g)
+        ? [...acc, elem.src]
+        : acc,
+    []
+  );
+
+  // iterate through all the scripts and
+  // fetch each one until finding the clientId
+  for (const src of scriptSources) {
+    const res = await fetch(src);
+    const data = await res.text();
+
+    const match = data.match(new RegExp(",client_id:\"(.*)\",env:\"production\""));
+    const clientId = match?.find((str) => str.length === 32);
+
+    if (clientId) {
+      window.SCDL__CLIENT_ID = clientId;
+
+      return;
+    }
+  }
+
+  throw new Error("Couldn't find SoundCloud clientId...");
+};
+
+(async () => {
+  try {
+    await setClientId();
+
+    console.info(`Found SoundCloud clientId! ${window.SCDL__CLIENT_ID}`);
+
+    insertDownloadButtons();
+  } catch (err) {
+    console.error(err);
+  }
+})();
