b8938612630cdf3420b6b3db2fcabdb6481e7cee
nyo <22390520+nyo@users.noreply.github.com>
2025-08-20 02:06:32 +0200

    feat: update to manifest v3
    * upgrade manifest version from 2 to 3 and update version to 2.5
    * modify build.sh to include a build message and change file permissions
    * improve README with development instructions and clarify browser compatibility

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 version=$(cat manifest.json | sed -n 's/.*"version": *"\([^"]*\)".*/\1/p')
-build_name="scdl-$version.zip"
 
+echo "Building cross-browser extension..."
+build_name="scdl-$version.zip"
 if ! zip -r "$build_name" icons/ libs/ index.js manifest.json; then
     echo "Error: Failed to create archive: $build_name"
     exit 1
 fi
 
-echo "Archive created: $build_name"
+echo -e "\nExtension created: $build_name"
